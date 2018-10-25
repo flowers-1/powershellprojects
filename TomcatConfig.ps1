7ffe93b952a8914d264196ab2b0f55b999dccfe2
Paul Flowers <pflowers@idsgrp.com>
2018-10-25 14:52:48 -0500

    Added TODO comment section

--- TomcatConfig.ps1
+++ TomcatConfig.ps1
@@ -12,6 +12,10 @@ function Test-IsAdmin {
 #Check for installed version of Tomcat.
 $tmctversion = Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" | select DisplayName | where {$_.DisplayName -like "*Tomcat*"} | ft -hidetableheaders
 
+<#TODO Design a subroutine to validate the values assigned to @tmctversion are acutally valid. The below routine only tests for the presence
+  of a value. For instance, assigning a value of "FOO" will still result in the script outputting "BAR" and continuing, even though "FOO"
+  is not a valid value for the Tomcat software. While the below routine will work for a very basic test, more robust testing is required.
+#>
 if(!$tmctversion){
     Write-Host "Please install Apache Tomcat"
     break
