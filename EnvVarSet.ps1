d8d1645a1760b3e6a0ecce307cdcf8e097222ac8
Paul Flowers <pflowers@idsgrp.com>
2018-10-25 11:24:40 -0500

    Added Registry code for listing installed programs.

--- EnvVarSet.ps1
+++ EnvVarSet.ps1
@@ -1,4 +1,4 @@
-#Check for Admin rights on the current run. This could also be accomplished via #requires -RunAsAdministrator header.
+#Check for Admin rights on the current run. This could also be accomplished via #requires -RunAsAdministrator.
 <#
         .SYNOPSIS
             Checks if the current Powershell instance is running with elevated privileges or not.
@@ -17,3 +17,41 @@ function Test-IsAdmin {
         throw "Failed to determine if the current user has elevated privileges. The error was: '{0}'." -f $_
     }
 }
+
+<# Get a list of the installed programs on the local machine using registry keys for generation. Once the list is generated, output the
+   results to a formatted table assigned to variable @array. Take the variable and grep the values in the table, searching for anything relating
+   to the Oracle Java Platform.
+
+$array = @()
+
+foreach($pc in $computers){
+
+    $computername=$pc.computername
+
+#Define the variable to hold the location of Currently Installed Programs
+    $UninstallKey="SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+
+#Create an instance of the Registry Object and open the HKLM base key
+    $reg=[microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine',$computername)
+
+#Drill down into the Uninstall key using the OpenSubKey Method
+    $regkey=$reg.OpenSubKey($UninstallKey)
+
+#Retrieve an array of string that contain all the subkey names
+    $subkeys=$regkey.GetSubKeyNames()
+
+#Open each Subkey and use GetValue Method to return the required values for each
+    foreach($key in $subkeys){
+
+        $thisKey=$UninstallKey+"\\"+$key
+        $thisSubKey=$reg.OpenSubKey($thisKey)
+        $obj = New-Object PSObject
+        $obj | Add-Member -MemberType NoteProperty -Name "ComputerName" -Value $computername
+        $obj | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $($thisSubKey.GetValue("DisplayName"))
+        $obj | Add-Member -MemberType NoteProperty -Name "DisplayVersion" -Value $($thisSubKey.GetValue("DisplayVersion"))
+        $obj | Add-Member -MemberType NoteProperty -Name "InstallLocation" -Value $($thisSubKey.GetValue("InstallLocation"))
+        $obj | Add-Member -MemberType NoteProperty -Name "Publisher" -Value $($thisSubKey.GetValue("Publisher"))
+        $array += $obj
+    }
+}
+$array | Where-Object { $_.DisplayName } | select ComputerName, DisplayName, DisplayVersion, Publisher | ft -auto
