0c2fb4a40447743d05d4aa1e57122c18a72aa454
Paul Flowers <pflowers@idsgrp.com>
2018-11-01 13:51:06 -0500

    Added user interactive GUI dialogs and input validation.

--- IL10_Prerequisites/Libraries/Download-Requirements.ps1
+++ IL10_Prerequisites/Libraries/Download-Requirements.ps1
@@ -3,26 +3,60 @@
   2. Get-JavaInstalll;
 
 Both of the above functions, once created and pathed in the other scripts, will be able to be called as individuals instead of as a whole.
-The method of downloading the installer files is Background Intelligent Transfer Service (BITS). Using Bits removes the need to call explicit file paths on the destination and will instead automatically download the file and name it as called in the URL.
+The method of downloading the installer files is Background Intelligent Transfer Service (BITS). Using Bits removes the need to call explicit file paths on the destination and will instead automatically download the file and save it using the name in the URL.
 #>
 
-
-function Get-Tomcat86 {
-$filepath = Read-Host -Prompt "Please enter the location where Tomcat will be downloaded to"
-Start-BitsTransfer -source "http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34-windows-x86.zip" -Destination $filepath
+function Get-Tomcat {
+  #64 bit logic here
+    if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit") {
+    #Ask for user input in a GUI dialog box
+   [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+    $filepath = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the location you want to save Tomcat to", "Save As")
+    #Test entered path for existence. If it does not exist, create the directory structure
+    if(-Not(Test-Path -Path $filepath)){
+      New-Item -ItemType directory -Path $filepath
+      }
+    Start-BitsTransfer -Source "http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34-windows-x64.zip" -Destination $filepath
+    }
+  #32 bit logic here
+else {
+    #Ask for user input in a GUI dialog box
+    [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+    $filepath = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the location you want to save Tomcat to", "Save As")
+    #Test entered path for existence. If it does not exist, create the directory structure
+    if(-Not(Test-Path -Path $filepath)){
+      New-Item -ItemType directory -Path $filepath
+      }
+    Start-BitsTransfer -source "http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34-windows-x86.zip" -Destination $filepath
+    }
+    [System.Windows.MessageBox]::Show('Apache Tomcat has been downloaded to ' + $filepath,"Download Success")
 }
 
-function Get-Tomcat64 {
-$filepath = Read-Host -Prompt "Please enter the location where Tomcat will be downloaded to"
-Start-BitsTransfer -Source "http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34-windows-x64.zip" -Destination $filepath
+function Get-JavaRE {
+  if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit") {
+    #64 bit logic here
+    [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+    $filepath = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the location you want to save Tomcat to", "Save As")
+    #Test entered path for existence. If it does not exist, create the directory structure
+    if(-Not(Test-Path -Path $filepath)){
+      New-Item -ItemType directory -Path $filepath
+      }
+    Start-BitsTransfer -Source "<Insert Direct Download Link for JavaRE x64 Offline Installer>" -Destination $filepath
+  }
+  else {
+    #32 bit logic here
+    [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+    $filepath = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the location you want to save Tomcat to", "Save As")
+    #Test entered path for existence. If it does not exist, create the directory structure
+    if(-Not(Test-Path -Path $filepath)){
+      New-Item -ItemType directory -Path $filepath
+      }
+    Start-BitsTransfer -Source "<Insert Direct Download Link for JavaRE x86 Offline Installer>" -Destination $filepath
+  }
+    [System.Windows.MessageBox]::Show('Oracle Java Runtime Environment has been downloaded to ' + $filepath)
 }
 
-function Get-Java86 {
-$filepath = Read-Host -Prompt "Please enter the location where the Java Runtime will be downloaded to"
-Start-BitsTransfer -Source "<Insert Direct Download Link for JavaRE x86 Offline Installer>" -Destination $filepath
-}
+function Extract-Tomcat {
 
-function Get-Java64 {
-$filepath = Read-Host -Prompt "Please enter the location where the Java Runtime will be downloaded to"
-Start-BitsTransfer -Source "<Insert Direct Download Link for JavaRE x64 Offline Installer>" -Destination $filepath
+
 }
