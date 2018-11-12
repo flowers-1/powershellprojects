b978550563f43584907feb5c52de596c8663a2b6
Paul Flowers <pflowers@idsgrp.com>
2018-11-12 15:29:47 -0600

    Simplified and streamlined the Tomcat file download and extract logic.

--- IL10_Prerequisites/Function_Libraries/Download-Requirements.ps1
+++ IL10_Prerequisites/Function_Libraries/Download-Requirements.ps1
@@ -6,58 +6,24 @@ Both of the above functions, once created and pathed in the other scripts, will 
 The method of downloading the installer files is Background Intelligent Transfer Service (BITS). Using Bits removes the need to call explicit file paths on the destination and will instead automatically download the file and save it using the name in the URL.
 #>
 
-#Downloads either the 32 or 64-bit Apache Tomcat program from an official mirror, depending on the users OS Architecture, and saves it to a user specified location.
+#Downloads either the 32 or 64-bit Apache Tomcat program from an official mirror, depending on the users OS Architecture, and saves it to a user specified location and then extracts it to the same location.
 function Get-Tomcat {
-  #64 bit logic here
-    if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit") {
-    #Ask for user input in a GUI dialog box
+	if((gwmi win32_operatingsystem | Select osarchitecture).osarchitecture -eq "64-bit"){
     [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null;
-    $filepath = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the location you want to save Tomcat to", "Save As");
-    #Test entered path for existence. If it does not exist, create the directory structure
-    if(-Not(Test-Path -Path $filepath)){
-      New-Item -ItemType directory -Path $filepath > $null;
-      }
-    Start-BitsTransfer -Source "http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34-windows-x64.zip" -Destination $filepath;
-    }
-  #32 bit logic here
-else {
-    #Ask for user input in a GUI dialog box
+		$filepath = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the location you want to download and extract Tomcat to", "File Operation");
+		Start-BitsTransfer -Source "http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34-windows-x64.zip" -Destination $filepath
+		$source = Get-ChildItem -Path $filepath\* -Include apache *.zip
+		[io.compression.zipfile]::ExtractToDirectory(($source, $filepath))
+	}
+	else {
     [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null;
-    $filepath = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the location you want to save Tomcat to", "Save As");
-    #Test entered path for existence. If it does not exist, create the directory structure
-    if(-Not(Test-Path -Path $filepath)){
-      New-Item -ItemType directory -Path $filepath > $null;
-      }
-    Start-BitsTransfer -source "http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34-windows-x86.zip" -Destination $filepath;
-    }
-    [System.Windows.MessageBox]::Show('Apache Tomcat has been downloaded to ' + $filepath,"Download Success");
-}
+		$filepath = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the location you want to download and extract Tomcat to", "File Operation");
+		Start-BitsTransfer -Source "http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34-windows-x86.zip" -Destination $filepath
+		$source = Get-ChildItem -Path $filepath1\* -Include apache *.zip
+		[io.compression.zipfile]::ExtractToDirectory(($source, $filepath))
+	}
+	[System.Windows.MessageBox]::Show('Apache Tomcat has been downloaded and successfully extracted to ' + $filepath,"Download Success");
 
-#Extracts the downloaded Apache Tomcat program to a user specified location. TODO Code both 32 and 64-bit logic for extraction.
-function Extract-Tomcat {
-  if ((Get-CimInstance Win32_OperatingSystem).version -gt "6.1.7600"){ #Windows Server 2012 and higher logic here
-    Add-Type -assembly "system.io.compression.filesystem"
-    [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null;
-    $destination = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the location where you want to extract Tomcat to", "Unzip Location");
-    $downloadfile = Get-childitem -Path $filepath -Name "apache-*"
-    if(-Not(Test-Path -Path $destination)){
-      New-Item -ItemType directory -Path $destination > $null;
-      }
-    [io.compression.zipfile]::ExtractToDirectory($FilePath, $destination);
-    }
-  }
-else{ #Windows Server 2012 and lower here logic here
-  Add-Type -assembly "system.io.compression.filesystem"
-  [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null;
-  $destination = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the location where you want to extract Tomcat to", "Unzip Location");
-  $downloadfile = Get-childitem -Path $filepath -Name "apache-*"
-  if(-Not(Test-Path -Path $destination)){
-    New-Item -ItemType directory -Path $destination > $null;
-    }
-  [io.compression.zipfile]::ExtractToDirectory($FilePath, $destination);
-  }
-  [System.Windows.MessageBox]::Show('Apache Tomcat has been downloaded to ' + $filepath,"Download Success");
-}
 function Install-JavaRE {
   if((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit") {
   #Read working directory paths by using the user supplied path stored in @filepath
